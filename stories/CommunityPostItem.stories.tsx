6e8338dbaca1be8ed153638ce2a41aaec57459ee
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-07-05 10:13:12 -0600

    Create story for CommunityPostItem component

---
+++ stories/CommunityPostItem.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { View } from "react-native";
+import CommunityPostItem from "@/components/Cards/CommunityPostItem";
+
+const meta = {
+  title: "CommunityPostItem",
+  component: CommunityPostItem,
+  decorators: [
+    (Story) => (
+      <View style={{ padding: 16 }}>
+        <Story />
+      </View>
+    ),
+  ],
+} satisfies Meta<typeof CommunityPostItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    readStatus: true,
+    communityName: "Cut",
+    postTime: "9:40 AM",
+    postBody: "Hola comunidad, este espacio es para que nos dejen sus mensajes"
+  },
+};
+
+export const Example2: Story = {
+  args: {
+    readStatus: false,
+    communityName: "Futbol",
+    postTime: "11:00 AM",
+    postBody: "Hoy se arman las retas a las 3:00"
+  },
+};
