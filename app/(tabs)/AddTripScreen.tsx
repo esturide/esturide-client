d9f402627f5797926e870e113d207ba85114058b
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-08-07 00:36:14 -0600

    Refactor AddTripScreen styles. Edit background image to remove border

--- app/(tabs)/AddTripScreen.tsx
+++ app/(tabs)/AddTripScreen.tsx
@@ -4,6 +4,7 @@ import ButtonFilterGreen from "@/components/Buttons/ButtonFilterGreen";
 import BottomSheetBase from "@/components/Sheets/BottomSheetBase";
 import BottomSheet from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet";
 import { useRef } from "react";
+import styles from "@/styles/AddTripScreen";
 
 const bgImage = require('assets/addTripBgImage.png');
 
@@ -41,40 +42,3 @@ export default function AddTripScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  screenContainer: {
-    height: "100%",
-    backgroundColor: "#FFFFFF",
-    paddingHorizontal: 30,
-    gap: 20,
-    alignItems: "center",
-  },
-  adBanner: {
-    backgroundColor: "orange",
-    height: 50,
-    width: "100%",
-  },
-  bgImage: {
-    height: 200,
-    width: 250,
-    marginTop: 80,
-  },
-  textBold: {
-    fontSize: 19,
-    fontWeight: "700",
-    color: "#3d3d3d",
-    lineHeight: 23,
-    textAlign: "center",
-  },
-  text: {
-    fontSize: 16,
-    fontWeight: "400",
-    textAlign: "center",
-  },
-  btnContainer: {
-    gap: 30,
-    position: "absolute",
-    bottom: 30,
-    right: 30,
-  },
-});
---
+++ styles/AddTripScreen.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    height: "100%",
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 30,
+    gap: 20,
+    alignItems: "center",
+  },
+  adBanner: {
+    backgroundColor: "orange",
+    height: 50,
+    width: "100%",
+  },
+  bgImage: {
+    height: 200,
+    width: 250,
+    marginTop: 110,
+  },
+  textBold: {
+    fontSize: 19,
+    fontWeight: "700",
+    color: "#3d3d3d",
+    lineHeight: 23,
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  btnContainer: {
+    gap: 30,
+    position: "absolute",
+    bottom: 30,
+    right: 30,
+  },
+});
+
+export default styles;
