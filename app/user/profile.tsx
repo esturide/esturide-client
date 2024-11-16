0e86715a8ab9b95a340b41e0116221b2db2ebcc7
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-11-15 23:33:54 -0600

    Format files

--- app/user/profile.tsx
+++ app/user/profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { ProfileHeader } from './ProfileHeader';
 import { InfoSection } from './InfoSection';
 import { ActionItem } from './ActionItem';
