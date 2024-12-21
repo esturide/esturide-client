cb01564f17dc4ca15710506d3bf90cf1ffaf23bc
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-12-21 12:00:44 -0600

    Update SDK Expo and package of NPM

--- app.config.js
+++ app.config.js
@@ -7,9 +7,6 @@ export default ({ config }) => ({
   icon: './assets/icon.png',
   extra: {
     storybookEnabled: process.env.STORYBOOK_ENABLED,
-    eas: {
-      projectId: 'fa4d1cbd-cf6a-423f-94c0-590064c9ac6f',
-    },
   },
   splash: {
     image: './assets/splash.png',
@@ -18,7 +15,6 @@ export default ({ config }) => ({
   },
   updates: {
     fallbackToCacheTimeout: 0,
-    url: 'https://u.expo.dev/fa4d1cbd-cf6a-423f-94c0-590064c9ac6f',
   },
   assetBundlePatterns: ['**/*'],
   ios: {
