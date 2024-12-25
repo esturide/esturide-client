d53676584fc26b2643f650aef0084a4d89f26759
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-12-24 18:51:34 -0600

    Minor changes

---
+++ components/cards/credit/CreditCard.tsx
@@ -0,0 +1,75 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GenericModal } from '@components/modals/GenericModal';
+import React, { useState } from 'react';
+import styles, {
+  DefaultDriverColors,
+  DefautPassengerColor,
+} from '@styles/CreditCardList';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { CardInput } from '@components/forms/CardInput';
+
+export interface CreditCardInfo {
+  id: string;
+  cardLogo: string;
+  cardBank: string;
+  cardNumber: string;
+}
+
+export interface CreditCardListProps {
+  typeCard: 'driver' | 'passenger';
+  data: CreditCardInfo[];
+}
+
+const addCardIconStyles = {
+  driver: DefaultDriverColors.iconColor,
+  passenger: DefautPassengerColor.iconColor,
+};
+
+const addCardButtonStyles = {
+  driver: {
+    ...styles.addCardButton,
+    backgroundColor: DefaultDriverColors.addCardButton,
+  },
+  passenger: {
+    ...styles.addCardButton,
+    backgroundColor: DefautPassengerColor.addCardButton,
+  },
+};
+
+export const CreditCard = ({ data, typeCard }: CreditCardListProps) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const defaultIconSize = 18.38;
+
+  const changeVisibility = () => {
+    setShowModal(!showModal);
+  };
+
+  const onCloseModal = () => {};
+
+  const ModalCard = () => {
+    return (
+      <GenericModal isVisible={false} onClose={onCloseModal}></GenericModal>
+    );
+  };
+
+  return (
+    <TouchableOpacity onPress={changeVisibility}>
+      <View style={styles.addCardContainer}>
+        <View style={addCardButtonStyles[typeCard]}>
+          <MaterialCommunityIcons
+            name="plus"
+            size={defaultIconSize}
+            color={addCardIconStyles[typeCard]}
+          />
+        </View>
+        <Text>Agregar Tarjeta</Text>
+      </View>
+
+      <GenericModal isVisible={showModal} onClose={changeVisibility}>
+        <CardInput />
+      </GenericModal>
+
+      <ModalCard />
+    </TouchableOpacity>
+  );
+};
