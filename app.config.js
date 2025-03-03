a3aa046101cb21e420038d6dfc1fa9a795211007
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-03-02 21:27:15 -0600

    Minor changes in app.config.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
   ...config,
   name: 'EstuRide',
   slug: 'esturide',
-  owner: 'sealfreak',
+  owner: 'esturide',
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
